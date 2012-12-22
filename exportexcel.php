9155231074f5c13a627a79e4e1c3a97b3a352469
Xavier Dutoit <xavier@tttp.eu>
2012-12-23 00:02:37 +0100

    avoid duplicate headers

--- exportexcel.php
+++ exportexcel.php
@@ -1,20 +1,45 @@
 <?php
+  
+function writeHTMLFile($fileName, &$header, &$rows, $titleHeader = NULL, $outputHeader = TRUE) {
+  if ($outputHeader) {
+    header('Content-language: en');
+    CRM_Utils_System::download(CRM_Utils_String::munge($fileName),
+      'application/vnd.ms-excel',
+      CRM_Core_DAO::$_nullObject,
+      'xls',
+      FALSE
+    );
+    echo "<table><thead><tr>";
+    foreach ($header as $field) {
+      echo "<th>$field</th>";
+    }
+    echo "</tr></thead><tbody>";
+  }
+  $i = 0;
+  $fields_cnt = count($header);
+
+  foreach ($rows as $row) {
+    $schema_insert = '';
+    $colNo = 0;
+    echo "\n<tr>";
+    foreach ($row as $j => $value) {
+      echo "<td>" . htmlentities($value, ENT_COMPAT, 'UTF-8') . "</td>";
+    }
+    echo "</tr>";
+  }
+//  echo "</tbody></table>";
+}
 
 function exportexcel_civicrm_export( $exportTempTable, $headerRows, $sqlColumns, $exportMode ) {
 
   $writeHeader = true;
   $offset = 0;
-  $limit  = 200;
+  $limit  = 10;
 
-  $query = "
-    SELECT *
-    FROM   $exportTempTable
-    ";
+  $query = "SELECT * FROM $exportTempTable";
   require_once 'CRM/Core/Report/Excel.php';
   while ( 1 ) {
-    $limitQuery = $query . "
-      LIMIT $offset, $limit
-      ";
+    $limitQuery = $query . " LIMIT $offset, $limit;";
     $dao = CRM_Core_DAO::executeQuery( $limitQuery );
 
     if ( $dao->N <= 0 ) {
@@ -24,18 +49,18 @@ function exportexcel_civicrm_export( $exportTempTable, $headerRows, $sqlColumns,
     $componentDetails = array( );
     while ( $dao->fetch( ) ) {
       $row = array( );
-
       foreach ( $sqlColumns as $column => $dontCare ) {
         $row[$column] = $dao->$column;
       }
-
       $componentDetails[] = $row;
     }
-    CRM_Core_Report_Excel::writeHTMLFile( "Export_Records", $headerRows,
-        $componentDetails, null, $writeHeader );
+ 
+    //CRM_Core_Report_Excel::
+    writeHTMLFile( CRM_Export_BAO_Export::getExportFileName('xls', $exportMode)." ".date("Y-m-d_H-i"), $headerRows,$componentDetails, null, $writeHeader );
     $writeHeader = false;
     $offset += $limit;
   }
+  echo "</tbody></table>";
 
   CRM_Utils_System::civiExit( );
 }
